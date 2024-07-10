7133f949416aa7f75b5b2035a9ca7f1d2dae7ff6
snepal094 <nepalsuyasha@gmail.com>
2024-07-10 15:19:29 +0545

    mongo final

--- delete-operation/delete.mongodb.js
+++ delete-operation/delete.mongodb.js
@@ -1,6 +1,17 @@
 use("kec-crud");
 
 //TODO least 3 points on what u know about object ID
+//ObjectID
+/*
+A 4-byte timestamp, representing the ObjectID's creation, measured in seconds since the Unix epoch.
+A 5-byte random value generated once per process. This random value is unique to the machine and process.
+A 3-byte incrementing counter, initialized to a random value.
+
+Why use ObjectID?
+Unique Identification: Guarantees uniqueness across documents.
+Timestamp Embedded: Contains a timestamp, which can be useful for sorting by creation time.
+Compactness: Uses only 12 bytes, which is efficient compared to other unique ID schemes.
+*/
 
 //euta item hatdai ma delete hunna, update huncha
 //whole document delete huda actual delete operation bhako huncha
